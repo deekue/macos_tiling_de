8dd417a31d87211fcd3e2aab01687a6f0c15d67f
DQ <daniel@chaosengine.net>
2020-09-05 17:07:18 -0700

    updates to install.sh

--- install.sh
+++ install.sh
@@ -1,15 +1,25 @@
-#!/bin/sh
+#!/usr/bin/env bash
+#
+# QaD installer
 
 set -eEuo pipefail
 
-SRC="$(pwd)"
+SRC="$( cd -P "$(dirname -- "${BASH_SOURCE[0]}")" > /dev/null 2>&1 && pwd )"
 
-[ -r "Brewfile" ] && brew bundle || exit 1
+# check if SIP is enabled
+if command -v csrutil 2> /dev/null; then
+  csrutil status
+  # TODO if enabled show link to Yabai SIP doc
+fi
+
+# put skhd_parse.py in path
 if [ -d "$HOME/bin" ] ; then
   ln -svi "$SRC/bin/skhd_parse.py" "$HOME/bin/skhd_parse.py"
 else
   sudo ln -svi "$SRC/bin/skhd_parse.py" "/usr/local/bin/skhd_parse.py"
 fi
+
+# setup config files
 [ -d "$HOME/.config" ] || mkdir -p "$HOME/.config"
 for i in alacritty skhd yabai; do
   ln -svi "$SRC/config/$i" "$HOME/.config/$i"
@@ -19,7 +29,11 @@ done
 sed -i -e "s/USERNAME_GOES_HERE/${USER}/" "$HOME/.config/alacritty/alacritty"
 
 # tweaks
-bash "${SRC}/macos"
+source "${SRC}/macos"
+
+if [ -r "${SRC}/Brewfile" ]; then 
+  cd "${SRC}" && brew bundle
+fi
 
 brew services start yabai
 brew services start skhd
